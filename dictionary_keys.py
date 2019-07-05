4a18c44ebe1a08b170dd64e2302abc15ebed5293
Fahad Murtaza <fahdim@gmail.com>
2019-07-05 14:21:57 +0400

    Looping through keys in the dictionary

---
+++ dictionary_keys.py
@@ -0,0 +1,10 @@
+favorite_languages = {
+    'jen': 'python',
+    'sarah': 'c',
+    'edward': 'ruby',
+    'phil': 'python',
+}
+
+print("The following languages have been mentioned:")
+for person in favorite_languages.keys():
+    print(person.title())
