9e30bcc5fdfc64dc32fd6f2a5b59f32d883f5dfd
Fahad Murtaza <fahdim@gmail.com>
2019-07-05 14:19:25 +0400

    Adding set

---
+++ set.py
@@ -0,0 +1,11 @@
+favorite_languages = {
+    'jen': 'python',
+    'sarah': 'c',
+    'edward': 'ruby',
+    'phil': 'python',
+    'jimmy': 'English'
+}
+
+print("The following languages have been mentioned:")
+
+print(set(favorite_languages.values()))
